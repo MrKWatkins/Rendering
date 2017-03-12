adfa23b3c23e6e485c93eb15c928285e330ea690
MrKWatkins <mrkwatkins@gmail.com>
2017-03-12 12:29:41 +0000

    Added a factory method for Texture.

--- Source/MrKWatkins.Rendering.UI/MainForm.cpp
+++ Source/MrKWatkins.Rendering.UI/MainForm.cpp
@@ -39,8 +39,8 @@ namespace MrKWatkins::Rendering::UI
 
 		auto shadingModel = ShadingModel::Create<Lambertian>();
 
-		auto scene = std::make_unique<Scene::Scene>(Colour(0.2, 0.2, 0.2), std::shared_ptr<Texture>(std::make_shared<SkyGradient>(Colour(0.8, 1, 1), Colour(0, 0.2, 0.8))));
-		scene->AddPlane(Plane(Point(0, 0, 0), Vector(0, 1, 0)), std::shared_ptr<Texture>(std::make_shared<Chequerboard>(Colour(1, 1, 1), Colour(0, 1, 0), 0.25)));
+		auto scene = std::make_unique<Scene::Scene>(Colour(0.2, 0.2, 0.2), Texture::Create<SkyGradient>(Colour(0.8, 1, 1), Colour(0, 0.2, 0.8)));
+		scene->AddPlane(Plane(Point(0, 0, 0), Vector(0, 1, 0)), Texture::Create<Chequerboard>(Colour(1, 1, 1), Colour(0, 1, 0), 0.25));
 		scene->AddSphere(Sphere(0.5, 0.5, 0.5, 0.1), Colour(0, 0, 1));
 		scene->AddSphere(Sphere(0.2, 0.2, 1.25, 0.2), Colour(1, 0, 0));
 		scene->AddPointLight(Point(1, 1, 0), Attenuation::Linear(10), Colour(1, 1, 1));
--- Source/MrKWatkins.Rendering/Texture.h
+++ Source/MrKWatkins.Rendering/Texture.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 #include "Colour.h"
 #include "Point.h"
 
@@ -7,8 +8,17 @@ namespace MrKWatkins::Rendering::Textures
     class Texture
     {
     public:
+		template<typename TTexture, class... TConstructorArguments>
+		static std::shared_ptr<Texture> Create(TConstructorArguments&&... constructorArguments)
+		{
+			static_assert(std::is_base_of<Texture, TTexture>::value, "TTexture is not an instance of Texture.");
+
+			return std::shared_ptr<Texture>(new TTexture(std::forward<TConstructorArguments>(constructorArguments)...));
+		}
+
         virtual ~Texture() = default;
 
         virtual Colour GetColourAtPoint(const Geometry::Point& point) const = 0;
+
     };
 }
